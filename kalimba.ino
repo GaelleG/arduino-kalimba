25cd824d1ebe01b64f03d4d79e2babf179d452c0
Gaëlle Gomez <fazolg@gmail.com>
2019-07-09 21:13:45 +0200

    Kalimba simple main

--- kalimba.ino
+++ kalimba.ino
@@ -17,37 +17,9 @@
   https://github.com/GaelleG/arduino-kalimba
 */
 
-double frequency(int signal);
-double arpediator(int signal);
-double semitoneFrequency(int signal);
-double semitoneRound(double tone);
-
-const double ANALOG_MAX = 1024.;
-const double TONE_A1 = 220.;
-const double OCTAVE = 3.;
-
-#define QUOTIENT_A   0./12.
-#define QUOTIENT_AS  1./12.
-#define QUOTIENT_B   2./12.
-#define QUOTIENT_C   3./12.
-#define QUOTIENT_CS  4./12.
-#define QUOTIENT_D   5./12.
-#define QUOTIENT_DS  6./12.
-#define QUOTIENT_E   7./12.
-#define QUOTIENT_F   8./12.
-#define QUOTIENT_FS  9./12.
-#define QUOTIENT_G  10./12.
-#define QUOTIENT_GS 11./12.
-
-const double QUOTIENTS[] = {
-  QUOTIENT_A,
-  QUOTIENT_AS,
-  QUOTIENT_D,
-  QUOTIENT_F,
-  QUOTIENT_G
-};
-const int QUOTIENTS_SIZE = sizeof(QUOTIENTS) / sizeof(QUOTIENTS[0]);
-const int NOTES_SIZE = OCTAVE * QUOTIENTS_SIZE;
+#include "arpegiator.h"
+// #include "frequencies.h"
+// #include "semitones.h"
 
 double previousTone = 0.;
 
@@ -57,64 +29,11 @@ void setup() {
 
 void loop() {
   int sensorValue = analogRead(A0);
-  double frequencyValue = arpegiatorFrequency(sensorValue);
+  double frequency = getTone(sensorValue);
 
-  if (frequencyValue != previousTone) {
-    tone(9, frequencyValue, 100);
+  if (frequency != previousTone) {
+    tone(9, frequency, 50);
   }
 
-  previousTone = frequencyValue;
-}
-
-double frequency(int signal) {
-  // ----------------------
-  // |   A1 |   A2 |   A3 |
-  // |  110 |  220 |  440 |
-  // ----------------------
-  // Ax = pow(2,x-1)*110
-
-  // For 2 octaves A2 to A4
-  // freq = pow(2, x-1)*220
-  // --------------------------
-  // |   220 <   freq <   880 |
-  // |     0 <      x <     2 |
-  // |     0 < signal <  1024 |
-  // --------------------------
-  // x = signal*2/1024
-
-  double x = signal * 2. / ANALOG_MAX;
-
-  return pow(2., x - 1.) * TONE_A1;
-}
-
-double arpegiatorFrequency(int signal) {
-  // For 2 octaves of 7 notes
-  // ---------------------
-  // | 0 < signal < 1024 |
-  // | 0 < octave <    2 |
-  // | 0 <   note <   14 |
-  // ---------------------
-  // octave = signal*octaveSize/analogMax
-  // note = signal*notesSize/analogMax
-
-  int octave = floor(signal * OCTAVE / ANALOG_MAX);
-  int note = ceil(signal * NOTES_SIZE / ANALOG_MAX);
-  int quotientIndex = note - octave * QUOTIENTS_SIZE - 1;
-
-  double quotient = octave + QUOTIENTS[quotientIndex];
-
-  return pow(2., quotient - 1.) * TONE_A1;
-}
-
-double semitoneFrequency(int signal) {
-  double x = signal * 2. / ANALOG_MAX;
-  double rounded = semitoneRound(x);
-
-  return pow(2., rounded - 1.) * TONE_A1;
-}
-
-double semitoneRound(double x) {
-  double multiple = 12.;
-
-  return multiple * floor(((x * 100. + multiple / 2.) / multiple)) / 100.;
+  previousTone = frequency;
 }
