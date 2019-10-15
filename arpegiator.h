852ccd8f140ceab41b12930e8b2e1328ee2e3570
Gaëlle Gomez <fazolg@gmail.com>
2019-10-16 00:13:49 +0200

    Arpegiator tuning cleaning

--- arpegiator.h
+++ arpegiator.h
@@ -1,9 +1,5 @@
 #include "notes.h"
 
-int getTone(int signal, int sensorIndex);
-void setFrequencies();
-int getFrequency(double quotient);
-
 const int VOICES_COUNT = 3;
 const int NOTES_COUNT = 7 * 3;
 
@@ -81,18 +77,6 @@ const double VOICES[VOICES_COUNT][NOTES_COUNT] = {
 
 int frequencies[VOICES_COUNT][NOTES_COUNT];
 
-int getTone(int signal, int sensorIndex) {
-  // For 2 octaves of 14 notes
-  // ---------------------
-  // | 0 < signal < 1024 |
-  // | 0 <   note <   14 |
-  // ---------------------
-  // note = signal*notesSize/analogMax
-  int noteIndex = floor(signal * NOTES_COUNT / ANALOG_MAX);
-
-  return frequencies[sensorIndex][noteIndex];
-}
-
 void setFrequencies() {
   for (int i = 0; i < VOICES_COUNT; i++) {
     for (int j = 0; j < NOTES_COUNT; j++) {
@@ -102,5 +86,30 @@ void setFrequencies() {
 }
 
 int getFrequency(double quotient) {
-  return round(pow(2., quotient - 1.) * TONE_A1);
+  // The reference is the note A:
+  //   A0 →  55Hz
+  //   A1 → 110Hz
+  //   A2 → 220Hz
+  // Ax = 55 * pow(2, x)
+  //
+  // Given that an octave contains 12 notes
+  // Thus  0 <= quotient < 12
+  //   and x    = octave + quotient
+  //   and freq = pow(2, x) * 55
+  // e.g.  x    = 1
+  //   and freq = pow(2, 1) * 55 = 110 = A1
+  // e.g.  x    = 2 + 3 / 12
+  //   and freq = pow(2, 2 + 3 / 12) * 55 = 261,6256 = C2
+
+  return round(pow(2., quotient) * TONE_A0);
+}
+
+int getTone(int signal, int sensorIndex) {
+  // For 2 octaves of 14 notes:
+  //   0 < signal < 1024
+  //   0 <   note <   14
+  // Thus note = signal * notesSize / analogMax
+  int noteIndex = floor(signal * NOTES_COUNT / ANALOG_MAX);
+
+  return frequencies[sensorIndex][noteIndex];
 }
